068a8c852206e44a89c83ceb64eaa03b890d58f1
obahchimaobi <anthonyobah37@gmail.com>
2024-12-26 17:16:30 +0100

    Intial Commit

--- resources/views/livewire/comment-form.blade.php
+++ resources/views/livewire/comment-form.blade.php
@@ -4,10 +4,10 @@
         {{-- @csrf --}}
         <div class="grid grid-cols-full gap-3">
             <div
-                class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+                class="py-1 px-4 mb-4 bg-white rounded-lg rounded-t-lg border-slate-200 dark:bg-slate-800 dark:border-slate-700">
                 <label for="comment" class="sr-only">Your name</label>
                 <input type="text"
-                    class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
+                    class="px-0 w-full text-sm text-slate-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-slate-400 dark:bg-slate-800"
                     placeholder="Name (required)" name="commentor"
                     @auth value="{{ auth()->user()->name }}" @endauth
                     wire:model='commentor' required>
@@ -18,10 +18,10 @@
             </div>
         </div>
         <div
-            class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+            class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border-slate-200 dark:bg-slate-800 dark:border-slate-700">
             <label for="comment" class="sr-only">Your comment</label>
             <textarea id="comment" rows="6"
-                class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
+                class="px-0 w-full text-sm text-slate-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-slate-400 dark:bg-slate-800"
                 placeholder="Write a comment..."  name="comment" wire:model='comment' required></textarea>
 
             @error('comment')
