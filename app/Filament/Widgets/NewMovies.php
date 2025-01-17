ec666a1a9c8dd8c8a69574391bfefe1b87815f20
obahchimaobi <anthonyobah37@gmail.com>
2025-01-17 11:38:01 +0100

    Initial Commit

---
+++ app/Filament/Widgets/NewMovies.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Carbon\Carbon;
+use Filament\Tables;
+use App\Models\Movies;
+use Filament\Tables\Table;
+use Filament\Tables\Actions\Action;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Actions\BulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class NewMovies extends BaseWidget
+{
+    protected int|string|array $columnSpan = 'full';
+
+    protected static ?int $sort = 2;
+    
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                // ...
+                Movies::query()->whereDate('created_at', Carbon::today()),
+            )
+            ->columns([
+                // ...
+                // ...
+                Tables\Columns\TextColumn::make('name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('release_year'),
+                Tables\Columns\TextColumn::make('genres')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->badge()
+                    ->color(fn(string $state): string => match ($state) {
+                        'pending' => 'warning',
+                        'approved' => 'success',
+                    }),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                EditAction::make()
+                    ->form([
+                        Section::make()
+                            ->columns([
+                                'sm' => 3,
+                                'xl' => 6,
+                                '2xl' => 8,
+                            ])
+                            ->schema([
+                                TextInput::make('name')
+                                    ->disabled()
+                                    ->columnSpan([
+                                        'sm' => 2,
+                                        'xl' => 3,
+                                        '2xl' => 4,
+                                    ]),
+
+                                TextInput::make('genres')
+                                    ->disabled()
+                                    ->columnSpan([
+                                        'sm' => 2,
+                                        'xl' => 3,
+                                        '2xl' => 4,
+                                    ]),
+
+                                TextInput::make('runtime')
+                                    ->columnSpan([
+                                        'sm' => 2,
+                                        'xl' => 3,
+                                        '2xl' => 4,
+                                    ]),
+
+                                TextInput::make('trailer_url')
+                                    ->columnSpan([
+                                        'sm' => 2,
+                                        'xl' => 3,
+                                        '2xl' => 4,
+                                    ]),
+
+                                TextInput::make('download_url')
+                                    ->columnSpan([
+                                        'sm' => 2,
+                                        'xl' => 3,
+                                        '2xl' => 4,
+                                    ]),
+
+                                TextInput::make('vote_count')
+                                    ->columnSpan([
+                                        'sm' => 2,
+                                        'xl' => 3,
+                                        '2xl' => 4,
+                                    ]),
+
+                                Textarea::make('overview')
+                                    // ->disabled()
+                                    ->columnSpanFull()
+                            ])
+                    ]),
+                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\ForceDeleteAction::make(),
+                Tables\Actions\RestoreAction::make(),
+
+                Action::make('movies')
+                    ->label('Approve')
+                    ->color('success')
+                    ->icon('heroicon-m-check-circle')
+                    ->action(function ($record) {
+                        $record->update([
+                            'status' => 'approved',
+                            'approved_at' => Carbon::now(),
+                        ]);
+
+                        Notification::make()
+                            ->title('Approval Successful')
+                            ->success()
+                            ->send();
+                    })
+                    ->visible(fn($record) => $record->status === 'pending'),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+
+                    BulkAction::make('movies')
+                        ->label('Approve Selected')
+                        ->color('success')
+                        ->icon('heroicon-m-check-circle')
+                        ->action(function ($records) { // Use $records as a collection
+                            foreach ($records as $record) {
+                                $record->update([
+                                    'status' => 'approved',
+                                    'approved_at' => Carbon::now(),
+                                ]);
+                            }
+
+                            Notification::make()
+                                ->title('Approval Successful')
+                                ->success()
+                                ->send();
+                        })
+                ]),
+            ]);
+    }
+}
