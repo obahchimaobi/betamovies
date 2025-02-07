8605cbc8b4e792af6d5b6181ceb91a952a9fee9d
obahchimaobi <anthonyobah37@gmail.com>
2025-02-07 07:23:34 +0100

    Laravel Pint

--- resources/views/components/layouts/_header.blade.php
+++ resources/views/components/layouts/_header.blade.php
@@ -116,7 +116,7 @@
                         <button id="hs-dropdown-account" type="button"
                             class="size-[38px] inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent text-gray-800 focus:outline-none disabled:opacity-50 disabled:pointer-events-none dark:text-white"
                             aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
-                            <img class="shrink-0 rounded-full w-14 h-10"
+                            <img class="shrink-0 rounded-full w-8 h-8"
                                 src="{{ auth()->user()->avatar ? asset('storage/' . auth()->user()->avatar) : Avatar::create(auth()->user()->name)->toBase64() }}"
                                 alt="Avatar">
                         </button>
